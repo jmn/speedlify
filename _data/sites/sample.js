b8380eedb8bd7ba39305e1882e63d6e604fbef9d
Jussi Norlund <norlund@gmail.com>
2023-01-30 17:54:00 +0100

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
+	description: "Tested sites",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
