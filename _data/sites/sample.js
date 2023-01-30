22f822b55bae1b9647bdd4ba8ef7eea55a933ffc
Jussi Norlund <norlund@gmail.com>
2023-01-30 14:06:25 +0100

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -5,7 +5,6 @@ module.exports = {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://www.speedlify.dev/",
-		"https://astro.oldweb2.com/"
+		"https://www.speedlify.dev/"
 	]
 };
